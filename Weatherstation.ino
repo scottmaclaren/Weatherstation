c623d3a1127300fad43b79d98b7b8be915d00bbf
ScottM <ScottM@S-MB.local>
2017-09-24 22:35:21 -0700

    Parse for int works
    Only works for ints at the moment but it will correctly parse 2 digit
    numbers

--- Weatherstation.ino
+++ Weatherstation.ino
@@ -1,6 +1,7 @@
 //global variables
 
 bool newData = false;
+String inString = "";
 
 void setup() {
  int pinmin =2;
@@ -49,48 +50,60 @@ void loop() {
   const byte decimalsign = B1101111;
   const byte allLedsOff = B11111111;
 
-  byte topled;
-  byte middleled;
-  byte bottomled;
-  float incomingtemp;
-  float incomingtransfer;
+  static byte topled;
+  static byte middleled;
+  static byte bottomled;
+  static int incomingtemp;
+  static int incomingtransfer;
   float oldtemp;
   bool tempneg = false;
 
   
-  incomingtransfer = recvfloat();
-  
-  if (incomingtemp < 0){
-    tempneg = true;
+  if(Serial.available()>0){
+  //incomingtemp = Serial.readString();
+  //Serial.println("this is incoming" + incomingtemp);
+  //can't use serial int parse or else it returns 0 upon time out, which then wipes out the values.
+  //can't through exception as what if temp is zero
+
+
+  int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      Serial.print("Value:");
+      Serial.println(inString.toInt());
+      Serial.print("String: ");
+      Serial.println(inString);
+      incomingtemp = inString.toInt();
+      // clear the string for new input:
+      inString = "";
+    }
+    //parses int into individual digits
+    //there is also a .toFloat() function for later
+  newintparse(incomingtemp, &middleled, &bottomled);
   }
-  else
-  tempneg = false;
+  
+  
+  Serial.println(middleled);
+  Serial.println(bottomled);
+  delay(500);
 
   
-  if (newData = true){
-    incomingtemp = incomingtransfer;
-  }
-  
-  flipNewData();
-
   
 
   
 
-  //temperature now stored in incoming temp
 
-  floattodigit(incomingtemp, &topled, &middleled, &bottomled);
-  //now the seperated digits are stored in top middle and bottom
+  
 
-  //now need to write to display
+  
 
-  Serial.println("this is topled " + topled);
-  Serial.println("this is middleled " + middleled);
-  Serial.println("this is bottomled " + bottomled);
   
   
 
-  //numbertodisplay(topled,middleled,bottomled, true);
   
 
 
@@ -103,87 +116,111 @@ void loop() {
 
 //start of functions
 
-//worried about this getting stuck or returning weird things if the timing doesn't sync on rasp
-
-float recvfloat() {
- if (Serial.available() > 0) {
- newData = true;
- return Serial.parseFloat();
- }
-}
-
-
-void flipNewData() {
- if (newData == true) {
- newData = false;
- }
-}
 
 
 
-void floattodigit (float temp, byte * topled, byte * middleled, byte * bottomled){
-  //assuming nothing higher than 99.9 degress
-  //should I work with structs or pass it by reference?
-  //& to attachec address (put & in when I call function
-  // * to get actual value
-  if (temp > 99.9 && temp <1000) {
-    //cast to 3 digits
-    int intTemp = (int) temp;
+//new parsing functions
+void newintparse (int temp, byte * middleled, byte * bottomled){
+  if((temp>99) || (temp<-99)){
+
+    *middleled = 9;
+    *bottomled = 9;
     
-    *bottomled = (byte) (intTemp % 10);
-    intTemp = intTemp/10;
-
-    *middleled = (byte) (intTemp % 10);
-    intTemp = intTemp/10;
-    
-    *topled = (byte) (intTemp % 10);
-     
-
-    //call function for all 3 active, non zero
-    //could call it as like a state machine function or 4 seperate functions
   }
-//might get a type mismatch? Not sure what the mod op returns
-  else if (temp <= 99.9 && temp >= 10) {
-    //shift all into non decimal space
-    byte transfertemp = (byte) (temp * 10);
+
+  else if (temp == 0){
+
+     *middleled = 0;
+     *bottomled = 0;
+
     
-    *bottomled = (byte) (transfertemp % 10);
-    transfertemp = transfertemp / 10;
+  }
+  else{
+
+    *bottomled = temp % 10;
+    temp = temp / 10;
+    *middleled = temp;
     
-    *middleled = (byte) (transfertemp % 10);
-    transfertemp = transfertemp / 10;
-    
-    *topled = (byte) (transfertemp % 10);
+  }
   
-  //call function with one decimal place on middle led  
-  }
-
-
-  else if (temp > 0 && temp < 10){
-    
-//this is the binary for all LEDs off
-    *topled = B11111111;
-    //because we want the top to be off
-
-    byte transfertemp = (byte) (temp * 10);
-
-    *bottomled = (byte) (transfertemp % 10);
-    transfertemp = transfertemp / 10;
-    
-    *middleled = (byte) (transfertemp % 10);
-    
-    
-  }
-
-  else {
-    //for neg temperatures later to do
-    //turning them all off for the moment
-    *topled = B11111111;
-    *middleled = B11111111;
-    *bottomled = B11111111;
-  }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void floattodigit (float temp, byte * topled, byte * middleled, byte * bottomled){
+//  //assuming nothing higher than 99.9 degress
+//  //should I work with structs or pass it by reference?
+//  //& to attachec address (put & in when I call function
+//  // * to get actual value
+//  if (temp > 99.9 && temp <1000) {
+//    //cast to 3 digits
+//    int intTemp = (int) temp;
+//    
+//    *bottomled = (byte) (intTemp % 10);
+//    intTemp = intTemp/10;
+//
+//    *middleled = (byte) (intTemp % 10);
+//    intTemp = intTemp/10;
+//    
+//    *topled = (byte) (intTemp % 10);
+//     
+//
+//    //call function for all 3 active, non zero
+//    //could call it as like a state machine function or 4 seperate functions
+//  }
+////might get a type mismatch? Not sure what the mod op returns
+//  else if (temp <= 99.9 && temp >= 10) {
+//    //shift all into non decimal space
+//    byte transfertemp = (byte) (temp * 10);
+//    
+//    *bottomled = (byte) (transfertemp % 10);
+//    transfertemp = transfertemp / 10;
+//    
+//    *middleled = (byte) (transfertemp % 10);
+//    transfertemp = transfertemp / 10;
+//    
+//    *topled = (byte) (transfertemp % 10);
+//  
+//  //call function with one decimal place on middle led  
+//  }
+//
+//
+//  else if (temp > 0 && temp < 10){
+//    
+////this is the binary for all LEDs off
+//    *topled = B11111111;
+//    //because we want the top to be off
+//
+//    byte transfertemp = (byte) (temp * 10);
+//
+//    *bottomled = (byte) (transfertemp % 10);
+//    transfertemp = transfertemp / 10;
+//    
+//    *middleled = (byte) (transfertemp % 10);
+//    
+//    
+//  }
+//
+//  else {
+//    //for neg temperatures later to do
+//    //turning them all off for the moment
+//    *topled = B11111111;
+//    *middleled = B11111111;
+//    *bottomled = B11111111;
+//  }
+//
+//}
 
 void numbertodisplay(byte topled, byte  middleled, byte bottomled, bool posorneg){
 
